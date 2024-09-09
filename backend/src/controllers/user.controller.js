687e798cdb34b6038ad6db498a2c6b53e3704e06
Anuraga Sahoo <anuragliku.2020@gmail.com>
2024-09-09 18:04:27 +0530

    add profile edit function in user.controler.js file and set cloudinary.js and data.js and isAuthenticate.middleware.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,7 +1,10 @@
 import { User } from "../models/user.model.js";
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import getDataUri from "../utils/datauri.js";
 
+
+// User Register
 export const register = async (req, res) => {
   try {
     const {username, email, password} = req.body;
@@ -30,14 +33,12 @@ export const register = async (req, res) => {
       success: true,
     })
 
-
-
-
   } catch (error) {
     console.log(error)
   }
 }
 
+// User Login
 export const login = async (req, res) => {
   try{
     const {email, password} = req.body;
@@ -47,6 +48,7 @@ export const login = async (req, res) => {
         success: false,
       })
     }
+
     let user = await User.findOne({email});
     if(!user){
         return res.status(401).json({
@@ -61,22 +63,89 @@ export const login = async (req, res) => {
         message: "incorrect email or password",
         success: false,
       })
-  }
+   }
     user = {
        _id: user._id,
        username: user.username, 
        email: user.email,
        profilePicture: user.profilePicture,
        bio: user.bio,
-        
+       followers: user.followers,
+       following: user.following,
+       posts: user.posts,
     }
-    const token =await jwt.sign({userId:user._id},process.env.SECRET_KEY, {expiresIn:'1d'})
+    const token = await jwt.sign({userId:user._id},process.env.SECRET_KEY, {expiresIn:'1d'})
 
     return res.cookie('token', token, {httpOnly:true, sameSite: 'strict', maxAge: 1*24*60*60*1000}).json({
       message: `Welcome! back ${user.username}`,
       success: true,
+      user
     })
 
+  }catch(error){
+    console.log(error)
+  }
+}
+
+//Account Logout
+export const logout = async (_, res) => {
+  try {
+    return res.cookie('token', "", {maxAge:0}).json({
+      message: 'Logged out Successfully.',
+      success: true,
+    })
+  } catch (error) {
+    console.log(
+      error
+    )
+  }
+} 
+
+// get profiles
+export const getProfile = async (req, res) => {
+  try {
+    const userId = req.params.id
+    let user = await User.findById(userId);
+    return res.status(200).json({
+      user,
+      success: true
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// edit profile
+
+export const editProfile = async (req, res) => {
+  try{
+    const userId = req.id;
+    const {bio, gender} = req.body;
+    const profilePicture = req.file;
+    let cloudResponse ;
+
+    if(profilePicture){
+      const fileUri = getDataUri(profilePicture);
+      cloudResponse = await cloudinary.uploader.upload(fileUri)
+    }
+    const user = await User.findById(userId)
+    if(!user){
+      return res.status(404).json({
+        message: "User not Found.",
+        success: false
+      })
+    }
+    if(bio) user.bio = bio
+    if(gender) user.gender = gender;
+    if(profilePicture) user.profilePicture = cloudResponse.secure_url
+
+    await user.save();
+
+    return res.status(200).json({
+      message: 'Profile Updated.',
+      success: true,
+      user
+    })
   }catch(error){
     console.log(error)
   }

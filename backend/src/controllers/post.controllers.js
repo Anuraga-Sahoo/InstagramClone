789d8067743c203e5f7477213ce166a3811ff8c2
Anuraga Sahoo <anuragliku.2020@gmail.com>
2024-09-22 23:26:29 +0530

    get each comment post and delete post

--- backend/src/controllers/post.controllers.js
+++ backend/src/controllers/post.controllers.js
@@ -2,6 +2,7 @@ import sharp from "sharp" // in this file we use sharp to optimized post image
 import cloudinary from "../utils/cloudinary"
 import { Post } from "../models/post.model.js"
 import { User } from "../models/user.model.js"
+import commentModel from "../models/comment.model.js"
 
 
 // add new post
@@ -54,8 +55,172 @@ export const addNewPost = async (req, res) => {
 
 export const getAllPost = async (req, res) => {
   try {
+    const posts = await Post.find().sort({createdAt:-1})
+    .populate({path:'author', select: 'username, profilePicture'})
+    .populate({
+      path:'comments', 
+      sort:{createdAt: -1},
+      populate:{
+        path:'author',
+        select: 'username, profilePicture'
+      }
+    })
+
+    return res.status(200).json({
+      posts,
+      success: true
+    })
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// get user Posts
+
+export const getUserPost = async (req, res)=>{
+  try {
+    const authorId = req.id;
+    const posts= await Post.find({author:authorId}).sort({createdAt:-1}).populate({
+      path:'author',
+      select:'username, profilePicture'
+    })
+
+    return res.status(200).json({
+      posts,
+      success: true
+    })
+
+  } catch (error) {
+     console.log(error)
+  }
+}
+
+// like Post
+
+export const likePost = async (req, res)=>{
+  try {
+    const likeKrneWalaUserKiId = req.id;
+    const postId = req.params.id
+    const post = await Post.findById(postId)
+    if(!post) return res.status(404).json({message: "Post not found", success: false});
+
+    // like logic
+    await post.updateOne({$addToSet: {likes: likeKrneWalaUserKiId}})
+    await post.save();
+    // implement socket.io for realtime notification
+
+    return res.status(200).json({message:'Post liked', success: true})
+
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+// dislike post
+
+// like Post
+
+export const dislikePost = async (req, res)=>{
+  try {
+    const dislikeKrneWalaUserKiId = req.id;
+    const postId = req.params.id
+    const post = await Post.findById(postId)
+    if(!post) return res.status(404).json({message: "Post not found", success: false});
+
+    // dislike logic
+    await post.updateOne({$pull: {likes: dislikeKrneWalaUserKiId}})
+    await post.save();
+    // implement socket.io for realtime notification
+
+    return res.status(200).json({message:'Post disliked', success: true})
+
     
   } catch (error) {
-    
+    console.log(error)
+  }
+}
+
+// add comment
+export const addComment = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const commentKarneWalaUserKiId = req.id;
+
+    const {text} = req.body;
+    const post = await Post.findById(postId)
+    if(!text) return res.status(400).json({message: "text is required", success:false})
+
+      const comment = await Comment.create({
+        text,
+        author: commentKarneWalaUserKiId,
+        post: postId
+      }).comment.populate({
+        path: 'author',
+        select: "username, profilePicture"
+      })
+      post.comments.push(comment._id)
+
+      await post.save()
+
+      return res.status(201).json({
+        message: "comment Added",
+        comment,
+        success: true
+      })
+
+  }catch(error){
+    console.log(error)
+  }
+}
+
+// get each comments post
+export const getCommentsOfPost = async(req, res) => {
+  try {
+    const postId = req.params.id
+
+    const comments = await Comment.find({post:postId}).populate('author', 'username', 'profilePicture')
+
+    if(!comments) return res.status(404).json({message: 'No comments found for this post', success: false})
+
+    return res.status(200).json({success:true, comments})
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const deletePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const authorId = req.id
+
+    const post = await Post.findById(postId);
+    if(!post) return res.status(404).json({message: "Post not found", success: false})
+
+      // check if the logged in user is the owner of the post
+
+      if(post.author.toString() != authorId) return res.status(403).json({message: "unauthorized"});
+
+      //delete post
+      await Post.findByIdAndDelete(postId);
+
+      //remove the post id from users post
+
+      let user = await User.findById(authorId);
+      user.posts = user.posts.filter(id => id.toString() != postId)
+      await user.save()
+
+      // delete associated comments
+      await Comment.deleteMany({post: postId})
+
+      return res.status(200).json({
+        success: true,
+        message: 'Post deleted'
+      })
+
+
+
   }
 }
